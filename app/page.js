f3d93375dcce7570e2616db7de6e6d15fe486b9d
Helen <hcardoso103@gmail.com>
2024-09-09 20:39:18 -0400

    modified:   app/page.js new file:   app/sign-in/[[...sign-in]]/page.js new file:   app/sign-up/[[...sign-up]]/page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import { AppBar, Container, Toolbar, Typography, Button, Box, Grid } from "@mui/
 
 export default function Home() {
   return(
-    <Container maxWidth="lg">
+    <Container maxWidth="xl">
       <Head>
         <title>Flashcard SaaS</title>
         <meta name="description" content="Create flashcard from your text"></meta>
@@ -16,8 +16,8 @@ export default function Home() {
         <Toolbar>
           <Typography variant="h6" style={{flexGrow: 1}}>Flashcard SaaS</Typography>
           <SignedOut>
-            <Button color="inherit">Login</Button>
-            <Button color="inherit">Sign Up</Button>
+            <Button color="inherit" href="/sign-in">Login</Button>
+            <Button color="inherit" href="/sign-up">Sign Up</Button>
           </SignedOut>
           <SignedIn>
             <UserButton />
@@ -29,8 +29,8 @@ export default function Home() {
         textAlign: 'center',
         my: 4,
       }}>
-        <Typography variant="h2">Welcome to Flashcard SaaS</Typography>
-        <Typography variant="h5">
+        <Typography variant="h2" gutterBottom>Welcome to Flashcard SaaS</Typography>
+        <Typography variant="h5" gutterBottom>
           {' '}
           The easiest way to make flashcards from your text
           </Typography>
@@ -39,12 +39,10 @@ export default function Home() {
           </Button>
       </Box>
       <Box sx={{my:6}}>
-        <Typography variant="h4" components="h2">
-          Features
-        </Typography>
-        <Grid contained spacing={4}>
+        <Typography variant="h4" gutterBottom>Features</Typography>
+        <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
-            <Typography variant="h6">Easy Text Input</Typography>
+            <Typography variant="h6" gutterBottom>Easy Text Input</Typography>
             <Typography>
               {' '}
               Simply input your text and let our software do the rest. Creating
@@ -53,7 +51,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs={12} md={4}>
-            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography variant="h6" gutterBottom>Smart Flashcards</Typography>
             <Typography>
               {' '}
               Our AI intelligently breaks down your text into concise flashcards,
@@ -62,7 +60,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs={12} md={4}>
-            <Typography variant="h6">Accessible Anywhere</Typography>
+            <Typography variant="h6" gutterBottom>Accessible Anywhere</Typography>
             <Typography>
               {' '}
               Access your flashcards from any device, at any time. Study on the
@@ -70,6 +68,44 @@ export default function Home() {
             </Typography>
           </Grid>
         </Grid>
+      </Box>
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4" gutterBottom>Pricing</Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+              <Box sx={{
+                p: 3,
+                border: '1px solid',
+                borderColor: 'grey.300',
+                borderRadius: 2
+              }}>
+                <Typography variant="h5" gutterBottom>Basic</Typography>
+                <Typography variant="h6" gutterBottom>$5 / month</Typography>
+                <Typography>
+                  {' '}
+                  Access to basic flashcard features and limited storage.
+              </Typography>
+              <Button variant="contained" color="primary" sx={{mt: 2}}>Choose basic</Button>
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Box sx={{
+                  p: 3,
+                  border: '1px solid',
+                  borderColor: 'grey.300',
+                  borderRadius: 2
+                }}>
+                  <Typography variant="h5" gutterBottom>Pro</Typography>
+                  <Typography variant="h6" gutterBottom>$10 / month</Typography>
+                  <Typography>
+                    {' '}
+                    Unlimited flashcards and storage, with priority support.
+                </Typography>
+                <Button variant="contained" color="primary" sx={{mt: 2}}>Choose basic</Button>
+                </Box>
+            </Grid>
+          </Grid>
       </Box>
       
     </Container>
---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,33 @@
+import { SignIn } from "@clerk/nextjs";
+import { AppBar, Button, Container, Toolbar, Typography, Box, Link } from "@mui/material";
+
+export default function SignUpPage(){
+    return <Container maxWidth="xl">
+        <AppBar position="static">
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    sx={{
+                        flexGrow: 1,
+                    }}
+                >
+                    Flashcard SaaS
+                </Typography>
+                <Button color="inherit">
+                    <Link href="/sign-in" passhref>
+                        Login
+                    </Link>
+                </Button>
+                <Button color="inherit">
+                    <Link href="/sign-up" passHref>
+                        Sign Up
+                    </Link>
+                </Button>
+            </Toolbar>
+        </AppBar>
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+            <Typography variant="h4">Sign in</Typography>
+            <SignIn/>
+        </Box>
+    </Container>
+}
